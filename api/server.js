89bcc3e610e1dba67f577c4cfd0c9f7bb0a5c9e3
Kyle Lovell <ky2vell@gmail.com>
2020-08-19 05:17:27 -0400

    endpoints added || mvp

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
-const express = require("express");
-
-const db = require("../data/dbConfig.js");
+const express = require('express');
+const accountRouter = require('../routes/accounts');
+const error = require('../middleware/error');
+const colors = require('colors');
 
 const server = express();
 
 server.use(express.json());
+
+// API Routes
+server.use('/api/accounts', accountRouter);
+
+// Error MiddleWare
+server.use(error);
 
 module.exports = server;
---
+++ routes/accounts.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const db = require('../data/dbConfig');
+
+const router = express.Router();
+
+// @desc    Get all accounts
+// @route   GET /api/accounts
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await db('accounts');
+
+    res.status(200).json({ count: accounts.length, data: accounts });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// @desc    Get single account
+// @route   GET /api/accounts
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const account = await db('accounts').where({ id }).first();
+
+    res.status(200).json({ data: account });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// @desc    Create account
+// @route   POST /api/accounts
+router.post('/', async (req, res, next) => {
+  try {
+    if (!req.body.name || !req.body.budget) {
+      return res.status(400).json({ message: 'Missing account data!' });
+    }
+
+    const id = await db
+      .insert({
+        name: req.body.name,
+        budget: req.body.budget
+      })
+      .into('accounts');
+
+    const account = await db('accounts').where({ id }).first();
+
+    res.status(201).json({ data: account });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// @desc    Update account
+// @route   PUT /api/accounts/:id
+router.put('/:id', async (req, res, next) => {
+  try {
+    if (!req.body.name || !req.body.budget) {
+      return res.status(400).json({ message: 'Missing account data!' });
+    }
+
+    const { id } = req.params;
+
+    await db('accounts').where({ id }).update({
+      name: req.body.name,
+      budget: req.body.budget
+    });
+
+    const account = await db('accounts').where({ id }).first();
+
+    res.status(200).json({ data: account });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// @desc    Delete account
+// @route   DELETE /api/accounts/:id
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const count = await db('accounts').where({ id }).del();
+
+    res.status(200).json({ message: `${count} record(s) has been deleted` });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
